72c36aa8b5a98520196ff90bf6324c719dc04249
Moritz Rumpf <Moritz Rumpf>
2016-11-22 15:13:22 +0100

    Creating a game and switching rounds
    With this file one can create a game and switch through the rounds of each player by using the endRound function.

---
+++ src/test.hs
@@ -0,0 +1,28 @@
+data Card = Card String
+    deriving Show
+data Player = Player {
+    deck :: [Card],
+    hand :: [Card]
+} deriving Show
+
+data Game = Game {
+    players :: [Player],
+    activePlayer :: Int
+} deriving Show
+
+getPlayers :: Game -> [Player]
+getPlayers g = players g
+
+getActivePlayer :: Game -> Player
+getActivePlayer g = (players g) !! (activePlayer g)
+
+getNextPlayerIndex :: Game -> Int
+getNextPlayerIndex g = (-) ((+) (-1) (length $ players g)) (activePlayer g)
+
+endRound :: Game -> Game
+endRound g = Game (players g) (getNextPlayerIndex g)
+
+
+player1 = Player [] [Card "test"]
+player2 = Player [] [Card "test2"]
+game = Game [player1,player2] 0
