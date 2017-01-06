ede09bb5aca3efb8bf90697d144b313bbc20a720
Moritz Rumpf <moritz.rumpf@gmail.com>
2017-01-06 17:32:54 +0100

    Primitive game loop that doesn't alter state

--- src/test.hs
+++ src/test.hs
@@ -1,3 +1,5 @@
+import Control.Monad.State.Lazy
+
 data Card = Card String
     deriving Show
 data Player = Player {
@@ -25,7 +27,23 @@ endRound g = Game (players g) (getNextPlayerIndex g)
 pass :: Game -> Game
 pass g = g
 
+actionStringToFunction :: String -> (Game -> Game)
+actionStringToFunction "end" = endRound
+actionStringToFunction "pass" = pass
+
 
 player1 = Player [] [Card "test"]
 player2 = Player [] [Card "test2"]
 game = Game [player1,player2] 0
+
+type Cod game = State game
+
+main =  do
+    putStr "Select action (pass/end): "
+    inp <- getLine
+    if inp=="exit" || inp=="q"
+        then putStrLn "bye"
+        else do 
+            let newG = actionStringToFunction inp game
+            putStrLn $ show $ newG
+            main
