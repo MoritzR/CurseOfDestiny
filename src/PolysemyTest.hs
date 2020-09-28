6eb137e044dccfcbb5c03bef8c0f20275bda3ff0
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-09-28 09:55:07 +0200

    Add a polysemy example
    Adding the polysemy dependency and playing around with it. The next step would be to use it in the existing code.

---
+++ src/PolysemyTest.hs
@@ -0,0 +1,26 @@
+module PolysemyTest where
+
+import Polysemy ( Sem, runM, Members )
+import Polysemy.State (State, evalState, put, get)
+import Polysemy.Trace ( trace, traceToIO, Trace )
+import Data.Function ((&))
+
+type CountdownState = State Int
+
+
+countdown :: Members [CountdownState, Trace] r => Sem r ()
+countdown = do
+    currentCounter <- get
+    let newCounter = currentCounter - 1
+    put newCounter
+    trace $ "new Counter: " ++ show newCounter
+    if newCounter == 0
+        then trace $ "Countdown completed!"
+        else countdown
+
+runCountdownIO :: Int -> IO ()
+runCountdownIO x = do
+    countdown                   -- Sem [CountdownState, Output String] ()
+        & (evalState x)         -- Sem [Output String] ()
+        & traceToIO             -- Sem [] ()
+        & runM                  -- IO ()
