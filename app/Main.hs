b4664ffa157fcb3e2fd55e2f58763cb5837da870
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-10-05 08:42:02 +0200

    Remove unused GameState initialization

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,13 @@
 module Main where
 import Game
-import DataTypes (GameState(..))
 import Data.Function ((&))
-import Polysemy.State (evalState)
 import Polysemy.Trace (traceToIO)
 import Polysemy.Input (runInputSem)
 import Polysemy (runM, embed)
 
 main :: IO ()
 main = do
-    let player1 = createPlayer "player1"
-    let player2 = createPlayer "player2"
-    let gs = GameState (player1,player2)
-    startGame -- Sem [Trace, Input String, Input Int]
+    startGame
         & traceToIO
         & runInputSem (embed getLine)
         & runInputSem (embed (fmap toInt getLine))
