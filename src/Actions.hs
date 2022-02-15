6ecdc312cae8d209448ac69beed3cb14003c691c
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-02-15 09:56:03 +0100

    Use pipeline notation instead of stateful helper
    It seems to look better by just using the pipeline notation, because one can see the sameness in both `over` statements more clearly

--- src/Actions.hs
+++ src/Actions.hs
@@ -6,7 +6,7 @@ import Data.Tuple (swap)
 import DataTypes
 import qualified GameIO as Gio
 import qualified Polysemy.State as S
-import PolysemyLens ((-=), (<>=))
+import PolysemyLens ((-=))
 
 resolve :: Action -> Game r ()
 resolve action = case action of
@@ -82,8 +82,10 @@ destroyOne cardLens = do
 draw :: PlayerLens -> Game r ()
 draw playerLens = do
   gs <- S.get
-  playerLens . #hand <>= topOfDeck playerLens gs
-  S.put $ over (playerLens . #deck) tail gs
+  gs
+    & over (playerLens . #hand) ((:) $ topOfDeck playerLens gs)
+    & over (playerLens . #deck) tail
+    & S.put
 
 topOfDeck :: PlayerLens -> GameState -> Card
 topOfDeck playerLens = head . (^. playerLens . #deck)
