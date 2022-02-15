7398d04ef42802ff491329611a8581a7d45a3c7d
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-02-15 09:05:33 +0100

    Apply hlint hints
    and format

--- src/Actions.hs
+++ src/Actions.hs
@@ -1,12 +1,12 @@
 module Actions (resolve, deleteFirst) where
 
-import DataTypes
-import Data.Tuple (swap)
-import Control.Lens ((^.), (^..), over)
-import PolysemyLens ((-=))
+import Control.Lens (over, (^.), (^..))
 import Data.Function ((&))
-import qualified Polysemy.State as S
+import Data.Tuple (swap)
+import DataTypes
 import qualified GameIO as Gio
+import qualified Polysemy.State as S
+import PolysemyLens ((-=))
 
 resolve :: Action -> Game r ()
 resolve action = case action of
@@ -22,84 +22,79 @@ resolve action = case action of
 
 endRound :: Game r ()
 endRound = do
-    applyTurnEnds
-    gs <- S.get
-    S.put $ changeCurrentPlayer gs
+  applyTurnEnds
+  gs <- S.get
+  S.put $ changeCurrentPlayer gs
 
 changeCurrentPlayer :: GameState -> GameState
 changeCurrentPlayer = over #players swap
 
 applyTurnEnds :: Game r ()
 applyTurnEnds = do
-    gs <- S.get
-    let actions = concat $ gs^..activePlayer . #field . traverse . #effects . #getOnTurnEnd
-    actions
-      & map resolve
-      & sequence
-    return ()
+  gs <- S.get
+  let actions = concat $ gs ^.. activePlayer . #field . traverse . #effects . #getOnTurnEnd
+  mapM_ resolve actions
 
 pass :: GameState -> GameState
 pass = id
 
 creaturePower :: Card -> Int
-creaturePower card = case card^. #cardType of
-    Creature power  -> power
-    Spell           -> 0 -- is a default a good idea here, or should this fail instead?
+creaturePower card = case card ^. #cardType of
+  Creature power -> power
+  Spell -> 0 -- is a default a good idea here, or should this fail instead?
 
 attack :: Card -> Card -> Game r ()
 attack target source = case compare targetPower sourcePower of
-    LT -> destroy (enemyPlayer. #field) target
-    GT -> destroy (activePlayer. #field) source
-    EQ -> do
-        destroy (enemyPlayer. #field) target
-        destroy (activePlayer. #field) source
-    where (targetPower, sourcePower) = (creaturePower target, creaturePower source)
+  LT -> destroy (enemyPlayer . #field) target
+  GT -> destroy (activePlayer . #field) source
+  EQ -> do
+    destroy (enemyPlayer . #field) target
+    destroy (activePlayer . #field) source
+  where
+    (targetPower, sourcePower) = (creaturePower target, creaturePower source)
 
 addToField :: Card -> Game r ()
 addToField card = do
-    gs <- S.get
-    S.put $ over (activePlayer. #field) (card:) gs
+  gs <- S.get
+  S.put $ over (activePlayer . #field) (card :) gs
 
 destroy :: CardLens -> Card -> Game r ()
 destroy cardLens card = do
-    gs <- S.get
-    S.put $ over cardLens (deleteFirst card) gs
+  gs <- S.get
+  S.put $ over cardLens (deleteFirst card) gs
 
 directAttack :: Card -> PlayerLens -> Game r ()
 directAttack _source targetPlayer = do
-    targetPlayer.playerHp -= 1
+  targetPlayer . playerHp -= 1
 
 discardFromHand :: Card -> Game r ()
 discardFromHand card = do
-    gs <- S.get
-    S.put $ over (activePlayer. #hand) (deleteFirst card) gs
+  gs <- S.get
+  S.put $ over (activePlayer . #hand) (deleteFirst card) gs
 
 destroyOne :: CardLens -> Game r ()
 destroyOne cardLens = do
-    gs <- S.get
-    gs^.cardLens
-        & map (Destroy cardLens)
-        & resolveChoose
+  gs <- S.get
+  gs ^. cardLens
+    & map (Destroy cardLens)
+    & resolveChoose
 
 draw :: PlayerLens -> Game r ()
 draw playerLens = do
-    gs <- S.get
-    S.put $ over (playerLens. #deck) tail . over (playerLens. #hand) ((:) $ topOfDeck playerLens gs) $ gs
-
+  gs <- S.get
+  S.put $ over (playerLens . #deck) tail . over (playerLens . #hand) ((:) $ topOfDeck playerLens gs) $ gs
 
 topOfDeck :: PlayerLens -> GameState -> Card
-topOfDeck playerLens = head . (^.playerLens. #deck)
+topOfDeck playerLens = head . (^. playerLens . #deck)
 
 deleteFirst :: Eq a => a -> [a] -> [a]
 deleteFirst _ [] = []
-deleteFirst a (b:bc)
-    | a == b    = bc
-    | otherwise = b : deleteFirst a bc
+deleteFirst a (b : bc)
+  | a == b = bc
+  | otherwise = b : deleteFirst a bc
 
 resolveChoose :: [Action] -> Game r ()
 resolveChoose l = do
-    maybeChoice <- Gio.chooseOne l
-    Gio.logLn' . show $ maybeChoice
-    case maybeChoice of
-        Just choice -> resolve choice
-        Nothing -> return ()
+  maybeChoice <- Gio.chooseOne l
+  Gio.logLn' . show $ maybeChoice
+  mapM_ resolve maybeChoice
