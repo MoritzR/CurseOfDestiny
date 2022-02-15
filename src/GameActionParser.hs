799e6858e555e7be96f879e609f724caf02924b4
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-02-15 09:28:15 +0100

    apply hlint suggetions

--- src/GameActionParser.hs
+++ src/GameActionParser.hs
@@ -1,9 +1,16 @@
 module GameActionParser (parseGameAction) where
 
 import Control.Applicative ((<|>))
+import Data.Char (isDigit)
 import Data.Maybe (listToMaybe)
 import DataTypes
 import Text.ParserCombinators.ReadP
+  ( ReadP,
+    many1,
+    readP_to_S,
+    satisfy,
+    string,
+  )
 
 parseGameAction :: String -> Maybe GameAction
 parseGameAction = listToMaybe . map fst . readP_to_S gameAction
@@ -16,28 +23,24 @@ gameAction = foldl1 (<|>) [playFromHand, activateFromField, announceDirectAttack
 playFromHand :: ReadP GameAction
 playFromHand = do
   string "p"
-  i <- index
-  return $ PlayFromHand i
+  PlayFromHand <$> index
 
 activateFromField :: ReadP GameAction
 activateFromField = do
   string "c"
-  i <- index
-  return $ ActivateFromField i
+  ActivateFromField <$> index
 
 announceDirectAttack :: ReadP GameAction
 announceDirectAttack = do
   string "d"
-  i <- index
-  return $ AnnounceDirectAttack i
+  AnnounceDirectAttack <$> index
 
 announceAttack :: ReadP GameAction
 announceAttack = do
   string "a "
-  i1 <- index
+  i <- index
   string " "
-  i2 <- index
-  return $ AnnounceAttack i1 i2
+  AnnounceAttack i <$> index
 
 pass :: ReadP GameAction
 pass = do
@@ -52,7 +55,7 @@ end = do
 -- helper methods
 
 digit :: ReadP Char
-digit = satisfy (\char -> char >= '0' && char <= '9')
+digit = satisfy isDigit
 
 index :: ReadP Int
 index = fmap (minus1 . read) (many1 digit)
