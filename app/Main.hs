0a656c671c66f13d159055ab9d35d6cc18c9ca3d
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-12-13 13:28:29 +0100

    Fix deprecation warning

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 import Game
 import Data.Function ((&))
-import Polysemy.Trace (traceToIO)
+import Polysemy.Trace (traceToStdout)
 import Polysemy.Input (runInputSem)
 import Polysemy (runM, embed)
 
 main :: IO ()
 main = do
     startGame
-        & traceToIO
+        & traceToStdout
         & runInputSem (embed getLine)
         & runInputSem (embed readInt)
         & runM
