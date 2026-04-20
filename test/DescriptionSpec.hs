2abfd9aae75187b1c22a8268ac0e4fdb7e83039f
Moritz Andrich <moritz.rumpf@gmail.com>
2026-04-20 19:53:50 +0200

    Add golden tests for card descriptions

---
+++ test/DescriptionSpec.hs
@@ -0,0 +1,19 @@
+module DescriptionSpec where
+
+
+import Test.Hspec
+import Cards (series26)
+import DataTypesNew (Card)
+import Test.Hspec.Golden (golden)
+import Interpreter.Descriptor (describeCard)
+import Data.List (intercalate)
+
+spec :: Spec
+spec = do
+  describe "Golden tests for card descriptions" $ do
+    golden "descriptions of set 26" $
+      pure $ renderCards series26
+
+renderCards :: [Card] -> String
+renderCards set = intercalate "\n\n" $ describeCard <$> set
+
